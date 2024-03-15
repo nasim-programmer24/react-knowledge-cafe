1b19193ea78d11eb2157a670064a3aa554cf0588
Nasim <nasim.programmer24.com>
2024-03-15 21:12:35 +0600

    added images resources for the project and setup the project by react

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import Header from './components/Header/Header'
+
+function App() {
+
+  return (
+    <>
+      
+      <Header></Header>
+
+    </>
+  )
+}
+
+export default App
